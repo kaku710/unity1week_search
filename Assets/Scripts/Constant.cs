62c69f0c17bd1cba32cbea5d8f4bd528bfdf21b1
Takeshi Kaku <t.kaku710@gmail.com>
2019-10-17 02:39:04 +0900

    add getSuitablePosition method

--- Assets/Scripts/Constant.cs
+++ Assets/Scripts/Constant.cs
@@ -1,3 +1,7 @@
 ﻿internal static class Constant{
     public static readonly int CITIZEN_COUNT = 49;
+    public static readonly float MIN_X = 1f;
+    public static readonly float MAX_X = 67f;
+    public static readonly float MIN_Z = -49f;
+    public static readonly float MAX_Z = 19f;
 }
--- Assets/Scripts/StagePresenter.cs
+++ Assets/Scripts/StagePresenter.cs
@@ -3,19 +3,38 @@
 public class StagePresenter : MonoBehaviour {
     private Citizen criminal; //犯人を格納
 
-    private void Start(){
-        CreateCharacter();
+    private void Start () {
+        CreateCharacter ();
     }
 
     private void CreateCharacter () {
         for (int i = 0; i < 10; i++) {
-            var num = Random.Range(0,Constant.CITIZEN_COUNT);
-            var obj = (GameObject)Resources.Load("People/Citizen" + num);
-            var citizen = Instantiate(obj,Vector3.zero,Quaternion.identity);
-            if(i == 0){
-                criminal = citizen.GetComponent<Citizen>();
+            var num = Random.Range (0, Constant.CITIZEN_COUNT);
+            var obj = (GameObject) Resources.Load ("People/Citizen" + num);
+            var pos = GetSuitablePositon();
+            var citizen = Instantiate (obj, pos, Quaternion.identity);
+            if (i == 0) {
+                criminal = citizen.GetComponent<Citizen> ();
                 criminal.isCriminal = true;
             }
         }
+    }
+
+    public Vector3 GetSuitablePositon () {
+        Vector3 pos = Vector3.zero;
+        while (true) {
+            float rndX = Random.Range (Constant.MIN_X, Constant.MAX_X);
+            float rndZ = Random.Range (Constant.MIN_Z, Constant.MAX_Z);
+            Ray ray = new Ray (new Vector3 (rndX, 20f, rndZ), Vector3.down);
+            RaycastHit raycastHit;
+            if (Physics.Raycast (ray, out raycastHit, 30f)) {
+                if (raycastHit.collider.CompareTag ("Obstacle")) continue;
+                else if(raycastHit.collider.CompareTag ("Field")) {
+                    pos = new Vector3(rndX,0f,rndZ);
+                    break;
+                }
+            }
+        }
+        return pos;
     }
 }
